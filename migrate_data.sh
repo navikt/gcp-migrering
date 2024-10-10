0e0c332bbfa631d723a0a86270818aaadd44560d
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2024-10-10 11:57:03 +0200

    Tilpasninger for å slå sammen to baser for etterlatte

--- migrate_data.sh
+++ migrate_data.sh
@@ -1,46 +1,39 @@
 #!/bin/bash
 usage() {
 echo "---"
-echo "$0 <hostname where database resides> <database name> <database user> <database password> <gcp project> <gcp bucket> <gcp sql instance> <gcp database> <gcp db user> <service account email> <pg dump flags>"
+echo "$0 <hostname where database resides> <database name> <database user> <database password> <gcp project> <gcp bucket> <gcp sql instance> <gcp database> <gcp db user> <service account email>"
 }
 
-if [ "$#" -lt 9 ]; then
+if [ "$#" -lt 8 ]; then
   usage
   exit 1
 fi
 
+# der vi kopierer fra
 DB_HOST=$1
 DB_NAME=$2
 DB_USER=$3
 export PGPASSWORD=$4
-GCP_PROJECT=$5
-GCP_BUCKET=$6
-GCP_INSTANCE=$7
-GCP_DATABASE=$8
-GCP_DB_USER=$9
-GCP_SA_EMAIL=$10
-PG_DUMP_FLAGS=$11
 
-
+# der vi kopierer til
+GCP_PROJECT=$5 # etterlatte
+GCP_INSTANCE=$6 # etterlatte-behandling
+GCP_DATABASE=$7 # sakogbehandlinger
+GCP_DB_USER=$8 # postgres
+GCP_CONTEXT=$9
 
 # dump database
-pg_dump -h ${DB_HOST} -d ${DB_NAME} -U ${DB_USER} --format=plain --no-owner --no-acl ${PG_DUMP_FLAGS} -v \
+pg_dump -h ${DB_HOST} -d ${DB_NAME} -U ${DB_USER} --data-only --exclude-table-data=flyway_schema_history -v \
 	> /data/${DB_NAME}.dmp.gz
 
 # move database dump to bucket
 gcloud config set project ${GCP_PROJECT}
+# miljø (trenger vi denne ref hvordan vi spesifiserer databaser)
+gcloud config set context ${GCP_CONTEXT}
 gcloud auth activate-service-account --key-file /var/run/secrets/nais.io/migration-user/user
 
-gsutil mb -l EUROPE-NORTH1 gs://"${GCP_BUCKET}"
-gsutil iam ch serviceAccount:"${GCP_SA_EMAIL}":objectAdmin gs://"${GCP_BUCKET}"
-
-service_account_email=$(gcloud sql instances describe ${GCP_INSTANCE} | yq '.serviceAccountEmailAddress')
-gsutil iam ch serviceAccount:"${service_account_email}":objectViewer gs://"${GCP_BUCKET}"
-
-gsutil -m -o GSUtil:parallel_composite_upload_threshold=150M cp /data/${DB_NAME}.dmp.gz gs://"${GCP_BUCKET}"/
-
 # import database in gcp
-gcloud sql import sql ${GCP_INSTANCE} gs://${GCP_BUCKET}/${DB_NAME}.dmp.gz \
+gcloud sql import sql ${GCP_INSTANCE} /data/${DB_NAME}.dmp.gz \
   --database=${GCP_DATABASE} \
   --user=${GCP_DB_USER} \
   --quiet
